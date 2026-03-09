7b7b371299e620f2225ea29760c5235552ff4ae2
Baldwin <raphael.blaude@student.uclouvain.be>
2026-03-09 21:45:14 +0100

    MLP bithynion fin

--- classification/student/MLP.py
+++ classification/student/MLP.py
@@ -83,10 +83,20 @@ if MLP_amorium:
         # for i in range(len(y_test)):
         #     print(f"True label: {y_test[i]}, Predicted label: {y_pred[i]}, Predicted probabilities: chainsaw: {probas[i][0]}, fire: {probas[i][1]}, fireworks: {probas[i][2]}, gunshot: {probas[i][3]}")
 
-MLP_bithynion = True
+MLP_bithynion = False # Clôturé le lundi 9 mars 2026 à 21h40
 if MLP_bithynion:
+    """
+    Model description:
+    MLP model with 5 classes
+    Dataset: mcu13, vinikot, JBL Flip 5 - Auguste - spec_20_20 --> chainsaw, crackling fire, fireworks, gunshot, background
+             mcu13, fisher, local speakers - spec_20_20 --> chainsaw, crackling fire, fireworks, gunshot, background
+    Dataset équilibré
+    Application d'une PCA à 35 composants. Optimisation de l'architecture neuronale par
+    GridsearchCV. Meilleure architecture: (300, 300, 200, 100, 50)
+    Modèle clôturé le lundi 9 mars 2026 à 21h40
+    """
     GS_bithynion  = False
-    PCA_bithynion = True
+    PCA_bithynion = False
     Gen_bithynion = False
 
     records = [("mcu13", "fisher", "local speakers - spec_20_20"),         # 5 classes, 122 samples per class
@@ -99,7 +109,7 @@ if MLP_bithynion:
     if PCA_bithynion:
         # n_pca = [350, 200, 150, 100, 50, 25, 10, 5]
         n_pca = [40, 35, 30, 25, 20, 15]
-        hidden_layer_sizes = [(400, 400, 300, 200, 100, 50, 10), (300, 300, 200, 100, 50, 10), (200, 200, 100, 50, 10), (100, 100, 50, 10), (50, 50, 10), (20, 20, 10, 10, 5), (10, 10, 5, 5, 5)]
+        hidden_layer_sizes = [(200, 200, 100, 50, 10), (400, 300, 200, 100, 50, 10), (50, 50, 50, 25, 25)]
         scores = []
         params = []
         for i in range(len(n_pca)):
@@ -128,20 +138,49 @@ if MLP_bithynion:
         plt.savefig("PCA_bithynion_scores_vdd.pdf")
         plt.show()
 
+    if GS_bithynion:
+
+        pca = PCA(n_components=35)
+        x_train_pca = pca.fit_transform(x_train)
+        x_test_pca = pca.transform(x_test)
+
+        parameters = {
+            'hidden_layer_sizes': [(300, 300, 200, 100, 50, 10), (300, 300, 200, 100, 50), (300, 300, 200, 100, 50, 25), (300, 300, 200, 100, 100), (300, 300, 200, 100, 100, 50, 50), (200, 200, 100, 50, 10), (100, 100, 100, 100, 50, 50, 10)], #(400, 400, 300, 200, 100, 50, 10), (50, 50, 50, 25, 25, 10), (35, 35, 20, 20, 10), (300, 300, 200, 100, 50, 10, 5), (300, 300, 200, 100, 50, 10, 1),  (100, 100, 50, 10), 
+            'max_iter': [500], # Pas d'influence sensible sur les résultats
+            'random_state': [42],
+            "activation": ["relu"], # relu est clairement le meilleur
+            "learning_rate": ["constant"] # pas de différence entre constant et adaptive, on laisse le paramètre par défaut
+        }
+
+        hyperparameter_tuning = GridSearchCV(MLPClassifier(), parameters, cv=5, n_jobs=-1)
+        hyperparameter_tuning.fit(x_train, y_train)
+        pickle.dump(hyperparameter_tuning, open("hyperparameter_tuning bithynion 1.pkl", "wb"))
+
     if Gen_bithynion:
-        mlp = MLPClassifier(hidden_layer_sizes=(200, 200, 100, 50, 10), max_iter=500, random_state=42, activation="relu", learning_rate="constant")
+        # Print performances
+        mlp = MLPClassifier(hidden_layer_sizes=(300, 300, 200, 100, 50)	, max_iter=500, random_state=42, activation="relu", learning_rate="constant")
         mlp.fit(x_train, y_train)
         y_pred = mlp.predict(x_test)
         score = accuracy_score(y_test, y_pred)
         print(f"Test score: {score}")
         print("Confusion Matrix:")
         print(confusion_matrix(y_test, y_pred))
-        pickle.dump(mlp, open("MLP_bithynion.pkl", "wb"))
-        print("Model saved as MLP_bithynion.pkl")
         save_confusion_matrix(mlp, x_test, y_test, filename="confusion_matrix_bithynion.pdf", show=True)
 
+        # Save model on all the dataset
+        mlp.fit(data_normalized, labels)
+        pickle.dump(mlp, open("MLP_bithynion.pkl", "wb"))
+        print("Model saved as MLP_bithynion.pkl")
 
-# with open("hyperparameter_tuning amorium 1.pkl", "rb") as f:
+MLP_chios = False
+if MLP_chios:
+    records = [("mcu13", "vinikot", "JBL Flip 5 - Auguste - spec_20_20"), # chainsaw, crackling fire, fireworks, gunshot
+               ("mcu13", "fisher", "local speakers - spec_20_20")]   # background
+    data, labels = load_data(records)
+
+    data_normalized = data / np.linalg.norm(data, axis=1, keepdims=True)
+
+# with open("hyperparameter_tuning bithynion 1.pkl", "rb") as f:
 #     hyperparameter_tuning = pickle.load(f)
 
 # # Save all the mean_fit_time, param_activation, param_hidden_layer_sizes, param_learning_rate, param_max_iter ranked according to the mean_test_score in a md table
